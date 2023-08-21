a814fc89ef29a637230b1375ff6e99563b89c610
qycy <kyaruny@gmail.com>
2023-08-21 11:02:38 +0800

    完整的例子：generator

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,75 @@
+#include <concepts>
 #include <coroutine>
+#include <exception>
 #include <stdio.h>
+#include <string>
 
 using namespace std;
 
-struct Task {
-  // 作为协程返回值的R，它必须有个嵌套类型名为promise_type
+template <typename T> struct Generator {
+  struct promise_type;
+  using HandleType = coroutine_handle<promise_type>;
   struct promise_type {
-    // 该类型必须有一个函数签名为R get_return_object()的函数，
-    // get_return_object的返回值R将作为*协程函数*的返回值
-    Task get_return_object() {
-      return Task{
-          .handle = std::coroutine_handle<promise_type>::from_promise(
-              *this)}; // 固定模式？创建一个R，coroutine_handle用这个静态函数创建（获取）
+    Generator get_return_object() {
+      return Generator{HandleType::from_promise(*this)};
     }
-    suspend_never initial_suspend() { return {}; }
-    suspend_never final_suspend() noexcept { return {}; }  // 改成suspend_never会core
-    void unhandled_exception() {}
-    void return_void() { printf("co_return\n"); }
-    suspend_always yield_value(int v) {
-      value = v;
+    suspend_always initial_suspend() { return {}; }
+    suspend_always final_suspend() noexcept { return {}; }
+    void unhandled_exception() { exception = current_exception(); }
+    template <convertible_to<T> From> suspend_always yield_value(From &&from) {
+      value = static_cast<From &&>(from);
       return {};
     }
+    void return_void() {}
 
-    int value;
+    T value;
+    exception_ptr exception;
   };
 
-  coroutine_handle<promise_type> handle;
-  operator coroutine_handle<promise_type>() const { return handle; }
-  operator coroutine_handle<>() const { return handle; }
+  HandleType handle;
+
+  explicit Generator(HandleType h) : handle(h) {}
+  Generator(const Generator &) = delete;
+  ~Generator() { handle.destroy(); }
+  explicit operator bool() {
+    fetch();
+    return !handle.done();
+  }
+  T operator()() {
+    fetch();
+    valid_ = false;
+    return std::move(handle.promise().value);
+  }
+
+private:
+  bool valid_;
+  void fetch() {
+    if (!valid_) {
+      handle();
+      if (handle.promise().exception) {
+        rethrow_exception(handle.promise().exception);
+      }
+      valid_ = true;
+    }
+  }
 };
 
-template <typename PromiseType> struct Awaiter {
-  PromiseType *promise;
-  bool await_suspend(coroutine_handle<PromiseType> handle) {
-    promise = &handle.promise();
-    return false; // 返回false会阻止suspend，这样就能在获取promise指针后继续执行函数，在此期间我们可以给value第一次赋值
-  }
-  bool await_ready() { return false; } // 该函数返回true，则不会suspend函数
-  PromiseType *
-  await_resume() { // 如果不返回void而是一个值，该值作为co_await表达式的值
-    return promise;
-  }
-};
+Generator<string> split(std::string sentence, char c) {
+  size_t beg = 0;
 
-Task counter() {
-  for (int i = 0; i < 10; i++) {
-    co_yield i;
+  while (true) {
+    size_t pos = sentence.find(c, beg);
+    co_yield sentence.substr(beg, pos - beg);
+    if (pos == string::npos) {
+      break;
+    }
+    beg = pos + 1;
   }
-  co_return; // 返回空值调用p.return_void()，有值则为p.return_value(v)。当协程返回时，相当于隐式co_await
-             // final_suspend()，如果final_suspend不suspend当前函数，会自动销毁保存的状态，否则需要手动调用destory()
 }
 
 int main(int argc, char **argv) {
-  coroutine_handle<Task::promise_type> h =
-      counter(); // Task在这行后会被销毁，但coroutine_handle更像一个指针，所以Task的析构不影响我们获取的coroutine_handle的使用
-  while (!h.done()) {
-    printf("%d\n", h.promise().value);
-    h();
+  auto words_coro = split("Hello, C++ coroutine!", ' ');
+  while (words_coro) {
+    printf("%s\n", words_coro().c_str());
   }
-  h.destroy(); // 还需要手动释放空间
 }
